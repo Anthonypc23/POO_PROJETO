e2bbf1cc4c39e1cedfd1b176f1544d689f66e318
Wallace Silveira <wallace_s.g@hotmail.com>
2022-11-04 16:10:00 -0300

    Inclusão de tela Home, Cadastrar Professor, Cadastrar Aluno e Substituição da tela Pesquisar

--- Escola/src/utils/Main.java
+++ Escola/src/utils/Main.java
@@ -27,7 +27,7 @@ public class Main {
 //        controllerProfessor.Casdrastra(new Professor(controllerProfessor.LastID(),"Anthony Mukskdkdkdkkdkdkdito foda", 386420154, "02/06/2002", "Masculino",933689547, "anthony.Eusoufoda@PRacaralho.com", "Barramas", 487, "TI", 100184));
 //        controllerProfessor.Casdrastra(new Professor(controllerProfessor.LastID(),"Olga", 1, "02/06/2022", "M", 933689584, "anthony.richard2314@gmail.com", "Rua Da paz", 73, "Matematica", 4878.45));
 //        controllerProfessor.Casdrastra(new Professor(controllerProfessor.LastID(),"dani", 2, "02/06/2022", "M", 933689584, "anthony.richard2314@gmail.com", "Rua Da paz", 73, "Matematica", 4878.45));
-//        controllerProfessor.Casdrastra(new Professor(controllerProfessor.LastID(),"wALL", 3, "02/06/2022", "M", 933689584, "anthony.richard2314@gmail.com", "Rua Da paz", 73, "Matematica", 4878.45));
+//        controllerProfessor.Casdrastra(new Professor(controllerProfessor.LastID(),"WALL", 3, "02/06/2022", "M", 933689584, "anthony.richard2314@gmail.com", "Rua Da paz", 73, "Matematica", 4878.45));
 //        controllerProfessor.remover();
 //          controllerAluno.Casdrastra(new Aluno(controllerAluno.LastID(), 5.4, "Erick", 485684488, "02/15/16", "masculino", 933689584, "tony@gmail.com", "rua mendoça", 488, "Quimica"));
 //          controllerAluno.Casdrastra(new Aluno(controllerAluno.LastID(), 5.4, "Erick", 485684488, "02/15/16", "masculino", 933689584, "tony@gmail.com", "rua mendoça", 488, "Quimica"));
